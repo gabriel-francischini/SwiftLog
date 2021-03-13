92c2bebd49c573d6734bac0c374e5924cbb87f81
Gabriel F <empresagabriel@gmail.com>
2021-03-13 17:03:13 -0300

    Adicionando Maven minimamente funcional
    Tem alguns arquivos desnecessários que devem ser deletados em commits futuros

---
+++ src/main/java/br/com/ies/App.java
@@ -0,0 +1,13 @@
+package br.com.ies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

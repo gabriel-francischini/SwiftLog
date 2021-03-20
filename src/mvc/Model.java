4a365e5c85f95cf668291da68507d2788e1d0d5b
Gabriel F <empresagabriel@gmail.com>
2021-03-20 16:23:37 -0300

    Removendo pseudocódigo do MVC.Controller
    Já temos uma implementação do Padrão de Projetos Observador, então
    como esse pseudocódigo vai ficar já está definido. Infelizmente, por
    conta da falta de programação genérica do Java, não tem como
    especificar esse padrão pois depende dos tipos de eventos que um Model
    em específico vai propagar.

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -3,11 +3,6 @@ import br.com.ies.MVC.ModelReadOnly;
 import br.com.ies.MVC.Controller;
 
 class Model implements ModelReadOnly {
-    void registrarObservador(int evento, int código){
-    }
-
-    void notificarObservadores(int evento){
-    }
 
     // Métodos set...
     // void set(Controller.Chave chave);

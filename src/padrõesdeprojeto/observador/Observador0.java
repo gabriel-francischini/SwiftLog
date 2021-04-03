e4a1ea3e0be68aa2215b102fac5c0c4fd65e907d
Gabriel F <empresagabriel@gmail.com>
2021-04-03 06:33:16 -0300

    Arrumando bug de visibilidade do Design Pattern Observador

--- src/padrõesdeprojeto/observador/Observador0.java
+++ src/padrõesdeprojeto/observador/Observador0.java
@@ -2,6 +2,6 @@ package br.com.ies.padrõesdeprojeto;
 
 import br.com.ies.padrõesdeprojeto.Observável0;
 
-interface Observador0 {
+public interface Observador0 {
     public void notificar();
 }
--- src/padrõesdeprojeto/observador/Observador1.java
+++ src/padrõesdeprojeto/observador/Observador1.java
@@ -2,6 +2,6 @@ package br.com.ies.padrõesdeprojeto;
 
 import br.com.ies.padrõesdeprojeto.Observável1;
 
-interface Observador1<ArgType> {
+public interface Observador1<ArgType> {
     public void notificar(ArgType argumento);
 }
